1cd6e941be2aa3e5e21b004735070e734835eb8f
EreminDm <eremin.dm@gmail.com>
2015-09-15 00:13:35 +0600

    delete all dump

--- ej/public/journals/1/mobile.js
+++ ej/public/journals/1/mobile.js
@@ -1,9 +1,7 @@
 <script>
-function openMenu() {
-    var navButton = document.getElementsByClassName("fa fa-bars")[0];
-	navButton.onclick = function () {
-		var navBar = document.getElementById("navbar");
-		navBar.classList.toggle ("menu");
-	}
+var navButton = document.getElementsByClassName("fa fa-bars")[0];
+navButton.onclick = function () {
+	var navBar = document.getElementById("navbar");
+	navBar.classList.toggle ("open");
 }
 </script>
